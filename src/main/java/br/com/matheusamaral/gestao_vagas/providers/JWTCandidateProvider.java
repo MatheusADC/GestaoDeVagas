5c7b995c2d228a1278b6e859c0735c0568aa4a27
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-08-23 19:57:52 -0300

    fix: Erro 401 Unauthorized

--- src/main/java/br/com/matheusamaral/gestao_vagas/providers/JWTCandidateProvider.java
+++ src/main/java/br/com/matheusamaral/gestao_vagas/providers/JWTCandidateProvider.java
@@ -15,7 +15,7 @@ public class JWTCandidateProvider {
     private String secretKey;
 
     public DecodedJWT validateToken(String token) {
-        token = token.replace("Bearer", "");
+        token = token.replace("Bearer ", "");
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
@@ -30,3 +30,4 @@ public class JWTCandidateProvider {
         }
     }
 }
+
