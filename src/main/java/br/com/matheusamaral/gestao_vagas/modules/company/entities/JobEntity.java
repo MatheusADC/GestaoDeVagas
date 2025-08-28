dff0967331f20ace6a52891e578c52eeb4d5e201
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-08-28 17:26:08 -0300

    add: Exemplos para o Swagger em JobEntity

--- src/main/java/br/com/matheusamaral/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/matheusamaral/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,10 +30,15 @@ public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Schema(example = "Vaga para design", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "GYMPass, Plano de saúde", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
 
     @NotBlank(message = "Esse campo é obrigatório")
+    @Schema(example = "SENIOR", requiredMode = RequiredMode.REQUIRED)
     private String level;
 
     @ManyToOne
@@ -44,3 +51,5 @@ public class JobEntity {
     @CreationTimestamp
     private LocalDateTime createdAt;
 }
+
+
